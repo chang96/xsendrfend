e9c17013f34bbc8f99fc63e2be4868567c5c1c78
chang <rorewole@gmail.com>
2022-05-07 07:51:49 +0100

    added text and video elements

---
+++ src/elements/desctext/desctext.js
@@ -0,0 +1,9 @@
+function DescText(){
+
+    return <div>
+        <h>Text here...</h>
+    </div>
+}
+
+
+export default DescText
---
+++ src/elements/descvideo/descvideo.js
@@ -0,0 +1,9 @@
+function Vid({name, color, height, width, onclick, cN}){
+
+    return <div>
+        <video></video>
+    </div>
+}
+
+
+export default Vid
