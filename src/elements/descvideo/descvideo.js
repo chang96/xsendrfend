5027d0ecb3e45b3576b0641e0a8b920760de6f6e
rahman Orewole <rorewole@gmail.com>
2022-12-01 08:49:53 +0100

    fixed_desc_videos

--- src/elements/descvideo/descvideo.js
+++ src/elements/descvideo/descvideo.js
@@ -1,5 +1,8 @@
-import vidl from "../../assets/abstract.mp4"
-import vid from "../../assets/abstractx.mp4"
+let vidl ="https://res.cloudinary.com/codebono/video/upload/v1669878426/recipes/abstract_rpkuc9.mp4"
+let vid ="https://res.cloudinary.com/codebono/video/upload/c_scale,h_525,q_86/v1669878449/recipes/abstractx_vjonpi.mp4"
+let vid2 ="https://res.cloudinary.com/codebono/video/upload/v1669878449/recipes/abstractx_vjonpi.mp4"
+let bigScreen = window.matchMedia('screen and (min-width: 680px)')
+
 function Vid({name, color, height, width, onclick, cN}){
 
     return <div className="bg-white relative">
@@ -9,7 +12,7 @@ function Vid({name, color, height, width, onclick, cN}){
         }}>
         <video style={{
             
-        }} className="ml-24 origin-center rotate-[-25deg]" muted autoPlay loop height={""} width={"30%"} src= {vid}></video>
+        }} className="ml-24 origin-center rotate-[-25deg]" muted autoPlay loop height={""} width={"30%"} src= {!bigScreen.matches? vid : vid2}></video>
         </div>
 
         <video style={{
